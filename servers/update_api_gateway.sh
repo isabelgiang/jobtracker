6bcfa7d1b783133ead8945c6bbc7561951918210
Isabel Giang <isabelgiang@gmail.com>
2021-06-01 02:20:17 -0700

    stop containers before starting

--- servers/update_api_gateway.sh
+++ servers/update_api_gateway.sh
@@ -3,6 +3,9 @@
 docker pull hollowsunsets/api-gateway
 docker pull hollowsunsets/postgres
 
+# Remove all containers and stop all running services
+docker-compose down
 
+# Restart all containers and start all services again
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 exit 0
