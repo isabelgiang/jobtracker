13e685e96044083a574d4b5a181d96ce46a91d25
wsong6 <wsong6@uw.edu>
2021-06-02 01:02:41 -0700

    Point to correct applications.json file

--- clients/src/index.js
+++ clients/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import APPLICATIONS from './Data/Applications.json';
+import APPLICATIONS from './Data/applications.json';
 
 ReactDOM.render(<BrowserRouter><App applications={APPLICATIONS}/></BrowserRouter>, document.getElementById('root'));
 
